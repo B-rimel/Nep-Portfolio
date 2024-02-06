de18b153bf2fcb5031b907a322bb283ba19d4cdd
b-rimel <luca.afev78@gmail.com>
2024-02-06 18:39:20 +0100

    Nep portfolio

---
+++ index.js
@@ -0,0 +1,48 @@
+// index.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    var buttons = document.querySelectorAll('.graphic-button, .concept-button');
+    var conceptModal = document.getElementById('conceptModal');
+    var conceptImage = document.querySelector('.concept img');
+  
+    buttons.forEach(function (button) {
+      button.addEventListener('click', function () {
+        var filterClass = button.getAttribute('data-id');
+        toggleImages(filterClass);
+      });
+    });
+  
+    // Initial display (optional)
+    toggleImages('graphic');
+  
+    conceptImage.addEventListener('click', openConceptModal);
+    conceptModal.addEventListener('click', closeConceptModal);
+  });
+  
+  function toggleImages(filterClass) {
+    var allFigures = document.querySelectorAll('figure.graphic, figure.concept');
+  
+    allFigures.forEach(function (figure) {
+      if (figure.classList.contains(filterClass) || filterClass === 'all') {
+        figure.style.display = 'block';
+      } else {
+        figure.style.display = 'none';
+      }
+    });
+  }
+  
+  function openConceptModal() {
+    var conceptModal = document.getElementById('conceptModal');
+    conceptModal.style.display = 'flex'; // Display the modal
+  }
+  
+  function closeConceptModal(event) {
+    var conceptModal = document.getElementById('conceptModal');
+    var conceptImage = document.querySelector('.concept img');
+  
+    // Check if the click is outside the concept image in the modal
+    if (event.target === conceptModal && event.target !== conceptImage) {
+      conceptModal.style.display = 'none'; // Hide the modal
+    }
+  }
+  
